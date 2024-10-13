b805a97098aca75d18085c6fa0775c88626915f2
Braden Vail <bradenvail30@gmail.com>
2024-10-13 18:40:24 -0400

    Added variables and operate func

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const add = function (a, b) {
-  return a + b;
+let num1;
+let num2;
+let operator;
+
+const add = function (num1, num2) {
+  return num1 + num2;
 };
-const subtract = function (a, b) {
-  return a - b;
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
-const multiply = function (a, b) {
-  return a * b;
+const multiply = function (num1, num2) {
+  return num1 * num2;
 };
-const divide = function (a, b) {
-  return a / b;
+const divide = function (num1, num2) {
+  if (num2 === 0) {
+    return "Cannot divide by zero";
+  }
+  return num1 / num2;
 };
+
+const operate = function (num1, num2, operator) {};
